91a88959391ad479a0bfb7cf8a5054b2699efd8c
Alex <alex.arvedahl@gmail.com>
2017-11-23 11:54:11 +0100

    refactoring

--- employee.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class Employee:
-    def __init__(self,firstName,lastName):
-        self.firstName = firstName
-        self.lastName = lastName
-
-    def toJson(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify, json, request
-from employee import Employee
-import names
 import MySQLdb as mdb
-from models import Ingredient
+from models import Ingredient, Recipe
 
 
 app = Flask(__name__)
@@ -31,36 +29,6 @@ def mysql():
         ingre = Ingredient(row[0], row[1])
         ingredientList.append(ingre)
     return json.dumps([ing.__dict__ for ing in ingredientList])
-
-@app.route("/postjson", methods=['GET', 'POST'])
-def postjson():
-    request_json = request.get_json()
-    for item in request_json:
-        print(item.get("name"))
-
-    return "ok"
-
-@app.route("/json")
-def test():
-    employee = Employee('first name','last name')
-    return employee.toJson()
-
-@app.route("/jsonlist")
-def jsonlist():
-    list = []
-    employee = Employee('Alex','dkaldkal')
-    list.append(employee)
-    employee = Employee("Alex", "Arvedahl")
-    list.append(employee)
-    return json.dumps([employee.__dict__ for employee in list])
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return "User %s" %username
-
-@app.route('/user/<int:user_id>')
-def show_user_id(user_id):
-    return 'User id is %d' % user_id
 
 
 #export FLASK_APP=hello.py python -m flask run

9bb8cda79056b2d62e30a7522e8f84c5608e7283
Alex <alex.arvedahl@gmail.com>
2017-11-23 11:19:16 +0100

    Added recipe model

--- models.py
+++ models.py
@@ -12,6 +12,15 @@ class Ingredient:
     def __init__(self, id, name):
         self.id = id
         self.name = name
-         
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+class Recipe:
+    def __init__(self, recipeID, recipeName, rating):
+        self.recipeID = recipeID
+        self.recipeName = recipeName
+        self.rating = rating
+
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)

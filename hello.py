8210c4cb9c1a995444dc761dd2b9eb8695817a0f
Alex <alex.arvedahl@gmail.com>
2017-11-24 16:09:22 +0100

    returning list with json and refactoring

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, json, request
 import MySQLdb as mdb
 from models import Ingredient, Recipe
+import jsonpickle
+
 
 
 app = Flask(__name__)
@@ -13,26 +15,27 @@ con = mdb.connect(host="localhost",user="root",
 def mysql():
     request_json = request.get_json()
     recipeList = []
+
+    for row in select(request_json):
+        recipe = Recipe(row[0], row[1], 3, row[2])
+        ingre = Ingredient(row[3])
+        recipeList.append(recipe)
+        recipe.ingredients.append(ingre)
+
+    return jsonpickle.encode(recipeList)
+
+def select(requestparam):
     cur = con.cursor()
     dbquery=("SELECT recipes.recipeID, recipes.recipeName, recipes.ingNeed, ingredients.ingredientName  FROM ingredients " +
                "INNER JOIN ingredientsinrecipe ON ingredients.ingredientID = ingredientsinrecipe.ingredientsID " +
                "INNER JOIN recipes ON ingredientsinrecipe.recipeID = recipes.recipeID "+
                "WHERE ingredients.ingredientName ='Spaghetti'" )
-    if request_json:
-        for ingName in request_json:
+    if requestparam:
+        for ingName in requestparam:
             dbquery += "OR ingredients.ingredientName = '" + ingName.get("name") +  "'"
 
     dbquery += "ORDER BY recipes.recipeName"
     cur.execute(dbquery)
-    rows = cur.fetchall()
-    for row in rows:
-        recipe = Recipe(row[0], row[1], 3, row[2])
-        ingre = Ingredient(row[3])
-        recipeList.append(recipe)
-        recipe.ingredients.append(ingre)
-        #ingredientList.append(ingre)
-        # serialize ingredient object
-    return json.dumps([recipe.__dict__ for recipe in recipeList])
-
+    return cur.fetchall()
 
 #export FLASK_APP=hello.py python -m flask run

c67b546f6990be5fa6ecf437ae55c18df01810b1
Alex <alex.arvedahl@gmail.com>
2017-12-01 14:23:20 +0100

    Added event handler

--- frontend/src/app/pages/food/searchInput/searchInput.component.ts
+++ frontend/src/app/pages/food/searchInput/searchInput.component.ts
@@ -6,14 +6,22 @@ import { DataService } from '../data.service';
   templateUrl: './searchInput.html',
 })
 export class SearchInputComponent implements OnInit {
-
+    value = '';
     message: string;
+
     constructor(private data: DataService) { }
+
     ngOnInit() {
       this.data.currentMessage.subscribe(message => this.message = message);
     }
+
+// [(ngModel)]= "value"
+    onEnter() {
+      console.log(this.value);
+    }
+    // Ta emot det som står i fältet och då skapa ett nytt searchObject med det därefter lägga till det i en lista
     newMessage() {
-      this.data.changeMessage("Hello from Sibling");
+      this.data.changeMessage('Hello from Sibling');
     }
 
 }

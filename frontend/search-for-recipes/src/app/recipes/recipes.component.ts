05b20a08406c5c9f544c82e6540f1e733ecd99b4
Alex <alex.arvedahl@gmail.com>
2017-12-21 12:09:08 +0100

    Added mock list with recipes and details when clicking

--- frontend/search-for-recipes/src/app/recipes/recipes.component.ts
+++ frontend/search-for-recipes/src/app/recipes/recipes.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe';
+import { RECIPES }  from '../mock-recipes';
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+
+  recipes = RECIPES;
+  selectedRecipe: Recipe;
 
   recipe: Recipe = {
     recipeID: 1,
@@ -19,6 +24,10 @@ export class RecipesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(recipe: Recipe): void {
+    this.selectedRecipe = recipe;
   }
 
 }
